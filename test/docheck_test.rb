e2812cdec3c1a94cb60f83fc9aaea590b0fabcf6
kuntoaji <kunto.aji.kr@gmail.com>
2010-11-17 11:45:01 +0700

    add more test cases

--- test/docheck_test.rb
+++ test/docheck_test.rb
@@ -7,19 +7,46 @@ class TestDocheck < Test::Unit::TestCase
     end
   end
 
+  context "Docheck command" do
+    setup do
+      @docheck_bin = File.expand_path("../../bin/docheck", __FILE__)
+    end
+
+    should "shows version" do
+      assert_equal "Docheck #{Docheck::VERSION}\n", `ruby #{@docheck_bin} -v`
+      assert_equal "Docheck #{Docheck::VERSION}\n", `ruby #{@docheck_bin} --version`
+    end
+
+    should "executes domain name checker correctly" do
+      assert `ruby #{@docheck_bin} -n railsmine`
+      assert `ruby #{@docheck_bin} --name railsmine`
+    end
+
+    should "prints 'Invalid option'" do
+      assert_equal "Invalid option\n", `ruby #{@docheck_bin} -f`
+      assert_equal "Invalid option\n", `ruby #{@docheck_bin} --foo`
+      assert_equal "Invalid option\n", `ruby #{@docheck_bin} --foo bar`
+    end
+
+    should "prints 'Missing argument'" do
+      assert_equal "Missing argument\n", `ruby #{@docheck_bin} -n`
+      assert_equal "Missing argument\n", `ruby #{@docheck_bin} --name`
+    end
+  end
+
   context "A Docheck instance" do
     setup do
       @docheck = Docheck::Application.new('railsmine')
     end
 
-    should "respond to all its public instance method" do
+    should "responds to all its public instance method" do
       assert @docheck.respond_to?(:fetch)
       assert @docheck.respond_to?(:print)
       assert @docheck.respond_to?(:available_domains)
       assert @docheck.respond_to?(:registered_domains)
     end
 
-    should "return all availability and registered domain" do
+    should "returns all availability and registered domain" do
       result = @docheck.fetch
       assert result.is_a?(Array)
       assert result.first.is_a?(Array)
@@ -31,7 +58,7 @@ class TestDocheck < Test::Unit::TestCase
       assert_not_nil result
     end
 
-    should "run #print successfully" do
+    should "runs #print successfully" do
       assert @docheck.print 
     end
   end
